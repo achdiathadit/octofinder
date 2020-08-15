ea1a764a3825bcc34e80ab513d36edd0007deaad
achdiathadit <achdiat.dev@gmail.com>
2020-08-15 17:49:31 +0700

    Updates

---
+++ precache-manifest.9fc110dd6e6ddae7d8b126066486011d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e2343893992f8f10aaeeb4bd219d4d",
+    "url": "/octofinder/index.html"
+  },
+  {
+    "revision": "97cec931369e1fc2076d",
+    "url": "/octofinder/static/css/main.beef7374.chunk.css"
+  },
+  {
+    "revision": "448dab7ab302c9870269",
+    "url": "/octofinder/static/js/2.8d3b47b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/octofinder/static/js/2.8d3b47b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cec931369e1fc2076d",
+    "url": "/octofinder/static/js/main.89959a35.chunk.js"
+  },
+  {
+    "revision": "6156111d7f8a0ba844ca",
+    "url": "/octofinder/static/js/runtime-main.230f98e5.js"
+  }
+]);
